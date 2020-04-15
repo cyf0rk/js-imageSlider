3f792aaa54807069eafc9b668f8a4f1525b00c06
marijan111 <marijanbao@gmail.com>
2020-04-15 21:51:01 +0200

    learned about ajax

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const leftArrow = document.querySelector('.left-arrow');
 const rightArrow = document.querySelector('.right-arrow');
 const image = document.querySelector('#image');
+// Variable that gives us option on how much images we want to generate with ajax call in a function generateImages
 const numImagesToGenerate = 5;
-
-// Array that stores image url's
-let imageArr = [];
 
 arrowEvents();
 generateImages();
@@ -15,7 +13,7 @@ function arrowEvents() {
   rightArrow.addEventListener('click', rightSlide);
 }
 
-// Makes ajax request numImagesToGenerate number of times using Fetch API and push that promise url to imageArr
+// Makes ajax request (numImagesToGenerate) number of times using Fetch API and push that promise url to imageArr
 function generateImages() {
   for (let i = 0; i < numImagesToGenerate; i++) {
     fetch(`https://source.unsplash.com/800x400/?beach`).then((response) => {
@@ -24,17 +22,28 @@ function generateImages() {
   }
 }
 
+// Array that stores image url's
+let imageArr = [];
+// Current picture
+let picture;
+
 // Randomly chooses one image from imageArr to be displayed
 function chooseImage() {
-  picture = Math.floor(Math.random() * imageArr.length);
-  image.src = `${imageArr[picture]}`;
+  picture = imageArr[Math.floor(Math.random() * imageArr.length)];
+  image.src = `${picture}`;
+  image.style.transition = 'opacity 1s ease-in';
+  image.style.opacity = 1;
 }
 
 // Arrow functions that are called each time an arrow is clicked
 function leftSlide() {
-  chooseImage();
+  image.style.opacity = 0;
+  image.style.transition = 'opacity 1s ease-out';
+  setTimeout(chooseImage, 1000);
 }
 
 function rightSlide() {
-  chooseImage();
+  image.style.opacity = 0;
+  image.style.transition = 'opacity 1s ease-out';
+  setTimeout(chooseImage, 1000);
 }
